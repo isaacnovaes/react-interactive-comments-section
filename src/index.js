8c15cb58380a24d635c898bb696b46fbf3ca95ce
Isaac Novaes <isaac.novaes@hotmail.com>
2022-02-17 13:56:14 +0100

    Add CommentsDataProvider so that the whole App can access the context data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./reset.module.scss";
+import CommentsDataProvider from "./context/CommentsDataProvider.js";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<CommentsDataProvider>
+			<App />
+		</CommentsDataProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
